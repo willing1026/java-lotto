bd486e1823ddd9b074b3d68d60f895a334e8ecf3
Hwang Jaeseon <willing1026@gmail.com>
2020-12-16 22:47:21 +0900

    3단계 - 로또(2등) (#1103)
    * refactor: delete old sources
    
    * refactor: delete old sources
    
    * docs: step3 기능구현목록 작성
    
    * feat: 보너스 번호 당첨결과 추가, 기능 개선
    
    * 지난주 당첨티켓 객체 추가 (보너스 번호 관리)
    * matchCount를 숫자에서 enum 직접사용으로 변경
    * 이에 따른 객체 내 사용로직들 변경
    
    * refactor: assertEquals -> assertThat

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -41,4 +41,21 @@ public class LottoTickets {
     public int hashCode() {
         return Objects.hash(lottoTickets);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoTickets that = (LottoTickets) o;
+        return Objects.equals(lottoTickets, that.lottoTickets);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lottoTickets);
+    }
 }
